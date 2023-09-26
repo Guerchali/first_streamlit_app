8d6a1d7ea2029f99c2811c624470c35d23a7fbf5
Guerchali <66891963+Guerchali@users.noreply.github.com>
2023-09-26 10:08:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My parent New HEALThy diner')
 streamlit.header('hii everyone')
-streamlit.header(🥑🍞'Breakfast Menu')
-streamlit.text(🐔'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🥣'Hard-Boiled Free-Range Egg')
+streamlit.header('🥑🍞Breakfast Menu')
+streamlit.text('🐔Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥣Hard-Boiled Free-Range Egg')
