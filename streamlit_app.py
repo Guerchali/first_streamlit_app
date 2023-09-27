497687f51fe8ac64702d4a75a84e582c96a678b4
Guerchali <66891963+Guerchali@users.noreply.github.com>
2023-09-27 17:40:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()
+streamlit.text(fruityvice_response.json())
 streamlit.title('My parent New HEALThy diner')
 streamlit.header('hii everyone')
 streamlit.header('🥑🍞Breakfast Menu')
