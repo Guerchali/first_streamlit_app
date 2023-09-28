1071a6b0e6e42650c6161a217502310b392972b8
Guerchali <66891963+Guerchali@users.noreply.github.com>
2023-09-28 15:24:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 streamlit.title('My parent New HEALThy diner')
 streamlit.header('hii everyone')
 streamlit.header('🥑🍞Breakfast Menu')
