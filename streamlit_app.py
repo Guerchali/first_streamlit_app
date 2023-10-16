9889eac7e9b819642056cacd335b4ecceb6d2c0e
Guerchali <66891963+Guerchali@users.noreply.github.com>
2023-10-16 14:47:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,14 +19,15 @@ my_cnx = snowflake.connector.connect(**snowflake_params)
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
+streamlit.text(" the fruit list contains")
+streamlit.text(my_data_row)
 
 # Execute a SQL query
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 
 # Display the results using Streamlit
-streamlit.text(" the fruit list contains")
-streamlit.text(my_data_row)
+
 
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #my_cur = my_cnx.cursor()
