033d1d5a387cbbc15765ef00b6655f89368e59a2
Guerchali <66891963+Guerchali@users.noreply.github.com>
2023-10-16 14:49:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,9 @@ snowflake_params = {
 my_cnx = snowflake.connector.connect(**snowflake_params)
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.header(" the fruit list contains")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
 # Execute a SQL query
 
